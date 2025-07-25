52443e0bc5cae5fd3776e5ba80e30a1b4c26b8c3
bluntschli1821 <danielsdessy53@yahoo.com>
2025-07-25 23:45:02 +0100

    initial-commit

---
+++ src/components/search.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="/search.svg" alt="Search Icon" />
+        <input />
+      </div>
+    </div>
+  );
+};
